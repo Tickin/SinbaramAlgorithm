6576bca4fa725063aa035cfe79bd2186d6ec6b0c
Byeong Yeop Jang <all789789@pusan.ac.kr>
2015-11-14 01:36:14 +0900

    add SampleCode

---
+++ Study/SampleCode/Sample.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main(void){
+	/*****/
+	freopen("sample.inp", "r", stdin);
+	/*****/
+
+	int T, testCase;
+	cin >> T;
+
+	for (testCase = 0; testCase < T; testCase++){
+
+	}
+}
